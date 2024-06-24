71de50f3eda0c600f810b9943d5f5d310db1a990
VikaKlybik <129275783+VikaKlybik@users.noreply.github.com>
2024-06-24 22:33:56 +0300

    Feature/book service (#3)
    * create book-service
    
    * add resources
    
    * add resources

--- book-service/src/main/java/org/modsen/exception/ExceptionApiHandler.java
+++ book-service/src/main/java/org/modsen/exception/ExceptionApiHandler.java
@@ -1,10 +1,14 @@
 package org.modsen.exception;
 
 import org.modelmapper.spi.ErrorMessage;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class ExceptionApiHandler {
@@ -16,4 +20,26 @@ public class ExceptionApiHandler {
                 .body(new ErrorMessage(exception.getMessage()));
     }
 
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ErrorMessage> handleDuplicateKeyException(DuplicateKeyException exception) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorMessage(exception.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorMessage> handle(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorMessage(exception.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<ErrorMessage> handleAccessDeniedException(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ErrorMessage(exception.getMessage()));
+    }
 }
